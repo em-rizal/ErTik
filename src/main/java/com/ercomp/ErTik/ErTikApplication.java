56b366024269e175c7c60cadf40f29cb420a2086
em-rizal <rizaldotmail@gmail.com>
2020-08-29 07:39:27 +0700

    commit pertama

---
+++ src/main/java/com/ercomp/ErTik/ErTikApplication.java
@@ -0,0 +1,13 @@
+package com.ercomp.ErTik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErTikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErTikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ercomp/ErTik/ErTikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ercomp.ErTik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErTikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
